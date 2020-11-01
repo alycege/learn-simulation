91efe917a23e1761b9519014674bdaff3b89993f
Alyce Ge <alyce.ge@gmail.com>
2020-11-01 18:49:07 -0500

    need to fix enum types

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct BarrierOption {
 }
 
 fn main() {
-    let option_price = simulate_option_price(1000, 100.0, 10, 0.25, 5.0, 140.0);
+    let option_price = simulate_option_price(1000, 100.0, 10, 0.25, 5.0, 140.0, 145.0);
     println!("{:#?}", option_price);
 }
 
@@ -28,19 +28,17 @@ fn random_walk(n_steps: usize, mu: f64, sigma_sq: f64) -> std::vec::Vec<f64> {
     v
 }
 
-// fn terminal_option_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64, strike_price: f64, option_price: f64, barrier_price: f64) -> BarrierOption {
-//     let (final_price, price_vec) = generate_underlying_price(init_price, n_steps, mu, sigma_sq);
-//     let mut terminal = Option<BarrierResult>;
+fn option_price_terminal(price_vec: std::vec::Vec<f64>, final_price: f64, strike_price: f64, barrier_price: f64) -> BarrierResult {
 
-//     for &p in price_vec.iter() {
-//         // out price
-//         if p > option.barrier_price {
-//             terminal = Some(BarrierResult::Invalid);
-//         }
-//     };
+    for &p in price_vec.iter() {
+        // out price
+        if p > barrier_price {
+            return BarrierResult::Invalid;
+        }
+    };
 
-//     terminal
-// }
+    BarrierResult::TerminalOptionValue((final_price - strike_price).max(0.0));
+}
 
 fn generate_underlying_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64) -> (f64, std::vec::Vec<f64>) {
     let walk_vec = random_walk(n_steps, mu, sigma_sq);
@@ -55,15 +53,15 @@ fn generate_underlying_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq:
     (curr_price, price_vec)
 }
 
-fn simulate_option_price(num_iterations: usize, init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64, strike_price: f64) -> f64 {
+fn simulate_option_price(num_iterations: usize, init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64, strike_price: f64, barrier_price: f64) -> f64 {
     let mut results_vec = Vec::new();
 
     for _i in 0..num_iterations {
-        let final_price  = generate_underlying_price(init_price, n_steps, mu, sigma_sq);
+        let (underlying_price_expiry, price_vec)  = generate_underlying_price(init_price, n_steps, mu, sigma_sq);
 
         // Call option
-        let option_price = (final_price.0 - strike_price).max(0.0);
-        results_vec.push(option_price);
+        let option_final_price = option_price_terminal(price_vec, underlying_price_expiry, strike_price, barrier_price);
+        results_vec.push(option_final_price as f64);
     };
 
     let options_sum:f64 = results_vec.iter().sum();
