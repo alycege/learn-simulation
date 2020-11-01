fa02564c56bd3b6b06b707d173d90be12e0a7a0d
Alyce Ge <alyce.ge@gmail.com>
2020-11-01 17:42:08 -0500

    vanilla option pricing using monte carlo simulation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use rand_distr::{Distribution, Normal};
 use rand::thread_rng;
+use std::cmp::max;
 use structopt::StructOpt;
 
+enum BarrierResult {
+    TerminalOptionValue(f64),
+    Invalid
+}
+
+struct BarrierOption {
+    strike_price: f64,
+    option_price: f64,
+    barrier_price: f64,
+    terminal: Option<BarrierResult>
+}
+
 fn main() {
-    let mut final_val = simulate_price(50, 100.0, 10, 0.5, 1.5);
-    println!("{:#?}", final_val);
+    let option_price = simulate_option_price(1000, 100.0, 10, 0.25, 5.0, 140.0);
+    println!("{:#?}", option_price);
 }
 
 fn random_walk(n_steps: usize, mu: f64, sigma_sq: f64) -> std::vec::Vec<f64> {
@@ -15,35 +28,44 @@ fn random_walk(n_steps: usize, mu: f64, sigma_sq: f64) -> std::vec::Vec<f64> {
     v
 }
 
-fn option_price(init_price: f64, strike: f64, option_type: i64) -> f64 {
-    // enum for option price or knockout
-    //
-    let : u64 = match option_type {
-    };
+// fn terminal_option_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64, strike_price: f64, option_price: f64, barrier_price: f64) -> BarrierOption {
+//     let (final_price, price_vec) = generate_underlying_price(init_price, n_steps, mu, sigma_sq);
+//     let mut terminal = Option<BarrierResult>;
 
-}
+//     for &p in price_vec.iter() {
+//         // out price
+//         if p > option.barrier_price {
+//             terminal = Some(BarrierResult::Invalid);
+//         }
+//     };
 
+//     terminal
+// }
 
-
-fn underlying_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64) -> f64 {
+fn generate_underlying_price(init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64) -> (f64, std::vec::Vec<f64>) {
     let walk_vec = random_walk(n_steps, mu, sigma_sq);
     let mut curr_price = init_price;
-
+    let mut price_vec = Vec::new();
 
     for &price_delta in walk_vec.iter() {
         curr_price = curr_price + price_delta;
+        price_vec.push(curr_price);
     };
 
-    curr_price
+    (curr_price, price_vec)
 }
 
-fn simulate_price(num_iterations: usize, init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64) -> std::vec::Vec<f64> {
+fn simulate_option_price(num_iterations: usize, init_price: f64, n_steps: usize, mu: f64, sigma_sq: f64, strike_price: f64) -> f64 {
     let mut results_vec = Vec::new();
 
     for _i in 0..num_iterations {
-        let final_price = underlying_price(init_price, n_steps, mu, sigma_sq);
-        results_vec.push(final_price);
-    }
+        let final_price  = generate_underlying_price(init_price, n_steps, mu, sigma_sq);
 
-    results_vec
+        // Call option
+        let option_price = (final_price.0 - strike_price).max(0.0);
+        results_vec.push(option_price);
+    };
+
+    let options_sum:f64 = results_vec.iter().sum();
+    options_sum / num_iterations as f64
 }
